a790513104ebf8631b2aadae9fa8ea71db2f9bc4
Dmitriy0010 <152611563+Dmitriy0010@users.noreply.github.com>
2023-12-11 18:39:13 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,13 @@
+$(function(){
+    var mixer = mixitup('.portfolio_content');
+
+
+    
+
+})
+
+$('.slider-blog_inner').slick({
+    dots: true,
+     prevArrow: false,
+    nextArrow: false,
+});
